4b5123a72eb1f0e3d0fa475f1080b83d4ea5409f
NipunShrivats <nipunrawat16@gmail.com>
2025-09-24 23:03:19 +0530

    changes on sep24, change no.1

--- Readme.md
+++ Readme.md
@@ -14,3 +14,19 @@
 3. For Ex, in /search?page=4, req.query.page will give "page".
 4. Query parameters are often used to pass optional or filter data to the server withot modifying the route
 5. They are always part of the URL and visible in the browser address bar.
+
+## Form Submission
+
+1. You can use the <form> tag with action attribute to specify the URL to which the form data is submitted.
+2. By default, a form will use the GET request, and you can handle it with app.get to access form data via req.query, as data is sent through the URL as query strings.
+3. Since URL's have length limit, you can use the POSt method for form submisions, which sends the data in the request body, allowing for larger amount of data.
+4. To access data from a POST request, you must first use the express.urlencoded() middleware to parse the request.body
+5. The urlencoded option {extended:true} uses the "qs" library to parse the query string, allowing for more complex structure like nested objects, which the default parses cannot handle.
+
+## Handling 404 page
+
+1. Use a middleware function with no specific route, like app.use((req, res)=>{...}), to handle unmatched routes.
+2. Inside the middleware, send a 404 status uisng res.status(404) and a custom message or HTML response.
+3. Place this middleware after all defined routes to catch only unhandled requests
+
+` app.use((req, res)=>{res.status(404).send("Page not found");})` 
--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const app = express();
 
 // for using static files (a middleware)
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true })); // for POST
+// app.use(express.urlencoded()); // for POST
 
 // console.log("import.meta.dirname:", import.meta.dirname);
 // console.log("import.meta.url:", import.meta.url);
@@ -12,27 +14,46 @@ app.use(express.static("public"));
 // console.log(__filename.pathname);
 
 const homePage = path.join(import.meta.dirname, "public/index.html");
+const errorPage = path.join(import.meta.dirname, "views/404.html");
 console.log(homePage);
 app.get("/", (req, res) => {
   res.sendFile(homePage);
 });
 
 // -------- route Params ---------
-app.get("/profile/:title", (req, res) => {
-  console.log(req.params);
-  res.send(`<h1>This book is ${req.params.title}</h1>`);
-});
-app.get("/profile/:title/article/:username", (req, res) => {
-  console.log(req.params);
-  const formatedTitle = req.params.title.replace(/-/g, " ");
-  res.send(`<h1>This book is ${formatedTitle} by ${req.params.username}</h1>`);
+// app.get("/profile/:title", (req, res) => {
+//   console.log(req.params);
+//   res.send(`<h1>This book is ${req.params.title}</h1>`);
+// });
+// app.get("/profile/:title/article/:username", (req, res) => {
+//   console.log(req.params);
+//   const formatedTitle = req.params.title.replace(/-/g, " ");
+//   res.send(`<h1>This book is ${formatedTitle} by ${req.params.username}</h1>`);
+// });
+
+// // --------- query params (can use in searching) ---------
+// // example:- "http://localhost:8001/product?search=Samsung%20S24&limit=10"
+// app.get("/product", (req, res) => {
+//   console.log(req.query);
+//   res.send(`<h1>User is searching for ${req.query.search}</h1>`);
+// });
+
+// --------- form submission ---------
+// not correct
+// app.get("/contact", (req, res) => {
+//   console.log(req.query);
+//   res.redirect("/");
+//   // res.send("OK");
+// });
+
+app.post("/contact", (req, res) => {
+  console.log(req.body);
+  res.redirect("/");
 });
 
-// query params (can use in searching)
-// example:- "http://localhost:8001/product?search=Samsung%20S24&limit=10"
-app.get("/product", (req, res) => { 
-  console.log(req.query);
-  res.send(`<h1>User is searching for ${req.query.search}</h1>`);
+app.use((req, res) => {
+  // return res.status(404).send("<h1>page not found<h1/>");
+  return res.status(404).sendFile(errorPage);
 });
 
 app.listen(PORT, () => {
